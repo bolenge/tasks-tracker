0dcdb952fc469699d1a66b2cac533c734a55fda1
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2024-06-08 16:30:43 +0100

    feat: integration table

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,39 @@ import TheTopTask from './components/TheTopTask.vue'
         <div>
           <TheTopTask />
         </div>
+
+        <div class="mt-10 px-1">
+          <div class="overflow-x-auto">
+            <table class="table table-zebra">
+              <!-- head -->
+              <thead>
+                <tr>
+                  <th scope="col">Task</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <template v-for="(task) in 10" :key="task">
+                  <tr>
+                    <td class="w-[65%]">Hello world ! {{ task }}</td>
+                    <td class="">
+                      <span class="me-10">01/10/2022</span>
+                      <span class="me-10">00:10:30</span>
+
+                      <span class="btn btn-sm btn-outline font-light rounded-lg me-3">
+                        Edit
+                      </span>
+
+                      <span class="btn btn-sm btn-outline font-light btn-error rounded-lg">
+                        Delete
+                      </span>
+                    </td>
+                  </tr>
+                </template>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
